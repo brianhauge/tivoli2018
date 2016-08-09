f5fbb3368d7c7e024bed4ea4dac8b29a6c80a471
Brian Hauge Hansen <brian.hauge@gmail.com>
2016-08-09 16:52:17 +0200

    changes

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bhansen
+ * Date: 09/08/16
+ * Time: 16:46
+ */
+
+define(DBHOST,"localhost");
+define(DBUSER, "db");
+define(DBPASS, "db");
+define(DB, "tivoli2016");

ed704d0631213402f67af7a244eafd5c8e8dd8ae
Brian Hauge Hansen <brian.hauge@gmail.com>
2016-09-10 21:28:01 +0200

    t

--- classes/SmsScoreController.php
+++ classes/SmsScoreController.php
@@ -40,11 +40,12 @@ class SmsScoreController extends BaseInit
     }
     
     public function getScoreTableByGroup($group) {
-        $scoreTable = "<table class=\"table table-striped\"><thead><tr><th>Placering</th><th>Hold</th><th>Point</th></tr></thead><tbody>";
+        $scoreTable = "<table class=\"table table-striped\"><thead><tr><th>Placering</th><th>ID</th><th>Holdnavn</th><th>Point</th></tr></thead><tbody>";
         $counter = 1;
         foreach ($this->dbModel->getScore($group) as $score) {
             $scoreTable .= "<tr>";
-            $scoreTable .= "<th width='20%'>$counter</th>";
+            $scoreTable .= "<th width='10%'>$counter</th>";
+            $scoreTable .= "<td width='10%'>" . $score['cid'] . "</td>";
             $scoreTable .= "<td width='70%'>" . $score['team'] . "</td>";
             $scoreTable .= "<td><span class=\"badge\">" . $score['point'] . "</span></td>";
             $scoreTable .= "</tr>";

55930111848b166b03dbca6bea90085cc77061b0
Brian Hauge Hansen <brian.hauge@gmail.com>
2017-09-27 14:26:19 +0200

    changed password

--- schemas/users_20170927.sql
+++ schemas/users_20170927.sql
@@ -6,6 +6,6 @@ CREATE TABLE `tivoli2016_users` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `tivoli2016_users` (user,password) VALUES ("hauge",SHA2('11PVT8se',256));
+INSERT INTO `tivoli2016_users` (user,password) VALUES ("hauge",SHA2('test123',256));
 
 SELECT user FROM tivoli2016_users;

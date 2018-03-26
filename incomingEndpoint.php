b5051bd6c6df04836fe099de1a298ec23b21bc38
Brian Hauge Hansen <brian.hauge@gmail.com>
2018-03-26 15:58:58 +0200

    More Nexmo stuff

--- incomingEndpoint.php
+++ incomingEndpoint.php
@@ -43,6 +43,5 @@ if(SMSGW == 'app') {
 
 else if (SMSGW == "nexmo") {
     $dbModel = new DbModel();
-    $inBoundSMS = $dbModel->insertSMS($_REQUEST,'in');
-    file_put_contents('received.txt',$inBoundSMS);
+    $dbModel->insertSMS($_REQUEST,'in');
 }

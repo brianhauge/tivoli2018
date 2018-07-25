99a50bfb84956d26707358ca7e55e81e5fd2e5aa
Brian Hauge Hansen <brian.hauge@gmail.com>
2018-07-25 13:43:06 +0200

    t

--- classes/DbModel.php
+++ classes/DbModel.php
@@ -54,7 +54,7 @@ class DbModel extends BaseInit
      */
     public function insertCrew($name, $msisdn, $kreds, $comment = '') {
         $this->logger->info(__METHOD__.": $name, $kreds");
-        $stmt = $this->con->prepare("INSERT INTO tivoli2018_ncrew (name, mobile, kreds) VALUES (?, ?, ?, ?)");
+        $stmt = $this->con->prepare("INSERT INTO tivoli2018_ncrew (name, mobile, kreds, comment) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("ssss", $name, $msisdn, $kreds, $comment);
         $stmt->execute();
         return $stmt->sqlstate;
